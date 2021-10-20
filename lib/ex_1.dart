b14aaccd529fe5312d5f3801fb2b3b094e337ca6
dangqtphong <dangqtphong@gmail.com>
2021-10-20 22:58:25 +0700

    PhongDQT: Finish Ex1

---
+++ lib/ex_1.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+import './screen-value.dart';
+
+class ex_1 extends StatelessWidget {
+  const ex_1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenValue _screenValue = ScreenValue(context);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Container(
+                          height: _screenValue.rH(19),
+                          color: Colors.blue[300],
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Column(
+                                        children: [
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.grey[700],
+                                              )),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.orange,
+                                              )),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.blue[700],
+                                              )),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(flex: 2, child: Container())
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                        flex: 1,
+                                        child: Container(color: Colors.pink)),
+                                    Expanded(
+                                        flex: 1,
+                                        child: Container(
+                                            color: Colors.tealAccent[200])),
+                                    Expanded(
+                                        flex: 1,
+                                        child: Container(color: Colors.yellow))
+                                  ],
+                                ),
+                              )
+                            ],
+                          )),
+                      Container(
+                          height: _screenValue.rH(27), color: Colors.black),
+                      Container(
+                          height: _screenValue.rH(27), color: Colors.yellow),
+                    ],
+                  )),
+              Expanded(
+                flex: 7,
+                child: Container(color: Colors.pink),
+              ),
+              Expanded(
+                  flex: 4,
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(
+                        _screenValue.rW(2.2), 0, 0, 0),
+                    child: Container(color: Colors.pink),
+                  )),
+            ],
+          ),
+          Positioned(
+            bottom: _screenValue.rH(30),
+            left: _screenValue.rW(12),
+            child: Container(
+              height: _screenValue.rH(20),
+              width: _screenValue.rH(20),
+              color: Colors.black.withOpacity(0.4),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen-value.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ScreenValue {
+  final BuildContext? _context;
+  double? _height;
+  double? _width;
+  double? _heightPadding;
+  double? _widthPadding;
+
+  ScreenValue(this._context) {
+    MediaQueryData _queryData = MediaQuery.of(_context!);
+
+    _height = _queryData.size.height / 100.0;
+    _width = _queryData.size.width / 100.0;
+  }
+
+  double rH(double v) {
+    return _height! * v;
+  }
+
+  double rW(double v) {
+    return _width! * v;
+  }
+}
